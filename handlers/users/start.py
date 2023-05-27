acad86cb8c0d35a4c703c0a7d02490b125a4b7ff
sanjarbey <sanjarbeksharifboyevich@mail.ru>
2023-05-27 22:49:31 +0500

    django

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,23 @@
+import asyncpg
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp
+from keyboards.default.start_keyboard import menu
+from loader import dp, db
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    try:
+        user = await db.add_user(
+            telegram_id=message.from_user.id,
+            full_name=message.from_user.full_name,
+            username=message.from_user.username,
+        )
+    except asyncpg.exceptions.UniqueViolationError:
+        user = await db.select_user(telegram_id=message.from_user.id)
+
+    await message.answer(
+        "Xush kelibsiz! Do'konimizdagi mahsulotlarni ko'rish uchun quyidagi Menu tugmasini bosing",
+        reply_markup=menu,
+    )
